3b2ed7fe267a97b80c27eaba9d847d0bcef01117
Jeya <jeyapriyagopal@gmail.com>
2018-10-06 12:52:26 -0400

    updtaed panchayat with full assembly name.

--- AssemblyApp/UsrCtrl/ucPanchayat.Designer.cs
+++ AssemblyApp/UsrCtrl/ucPanchayat.Designer.cs
@@ -39,14 +39,14 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(23, 105);
+            this.dataGridView1.Location = new System.Drawing.Point(25, 105);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1206, 560);
+            this.dataGridView1.Size = new System.Drawing.Size(1206, 523);
             this.dataGridView1.TabIndex = 0;
             // 
             // cmbAssembly
             // 
-            this.cmbAssembly.DisplayMember = "AssemblyName";
+            this.cmbAssembly.DisplayMember = "AssemblyFullName";
             this.cmbAssembly.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbAssembly.FormattingEnabled = true;
             this.cmbAssembly.Location = new System.Drawing.Point(60, 78);
